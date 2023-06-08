65ab811a7d7d10db8e8213d1af0540cf065ed350
Moe <moe@Moes-MacBook-Pro.local>
2023-06-08 00:04:40 -0700

    index.html moved outside publuc

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -38,19 +38,19 @@ export const Skills = () =>{
                         <h2>
                             Skills
                         </h2>
-                        <p>This is just a dummy text that I will comeback to and change it up depending on whatever I want.<br></br></p>
+                        <p>This is <br></br></p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
                                     <img src={meter1} alt="img"/>
-                                    <h5>Web Development</h5>
+                                    <h5>Kotlin</h5>
                                 </div>
                                 <div className="item">
                                     <img src={meter2} alt="img"/>
-                                    <h5>Brand Identity</h5>
+                                    <h5>Javascript</h5>
                                 </div>
                                 <div className="item">
                                     <img src={meter3} alt="img"/>
-                                    <h5>Logo Design</h5>
+                                    <h5>React/React Native</h5>
                                 </div>
                                 <div className="item">
                                     <img src={meter1} alt="img"/>
